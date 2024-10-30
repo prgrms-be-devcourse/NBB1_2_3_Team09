349bf099f0ad0d9c6d085892c109b83f12f922f7
xxxkyeom <seonggyeomgim97@gmail.com>
2024-10-30 14:35:30 +0900

    feat: caretaker 레포지토리 마이그레이션

--- src/main/kotlin/medinine/pill_buddy/domain/user/caretaker/repository/CaretakerRepository.kt
+++ src/main/kotlin/medinine/pill_buddy/domain/user/caretaker/repository/CaretakerRepository.kt
@@ -3,5 +3,5 @@ package medinine.pill_buddy.domain.user.caretaker.repository
 import medinine.pill_buddy.domain.user.caretaker.entity.Caretaker
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CaretakerRepository: JpaRepository<Caretaker, Long> {
+interface CaretakerRepository: JpaRepository<Caretaker, Long>{
 }
