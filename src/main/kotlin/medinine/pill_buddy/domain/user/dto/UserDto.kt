0ed29ca6648babf6eb390c10df9aa07b92e5aef7
hongjeZZ <ajyong316@gmail.com>
2024-10-30 17:05:41 +0900

    feat: 회원 반환 DTO 클래스 구현

---
+++ src/main/kotlin/medinine/pill_buddy/domain/user/dto/UserDto.kt
@@ -0,0 +1,21 @@
+package medinine.pill_buddy.domain.user.dto
+
+import medinine.pill_buddy.domain.user.entity.User
+
+data class UserDto(
+    val userType: String,
+    val username: String,
+    val loginId: String,
+    val email: String,
+    val phoneNumber: String,
+    val imageUrl: String
+) {
+    constructor(user: User) : this(
+        userType = user::class.simpleName!!.lowercase(),
+        username = user.username,
+        loginId = user.loginId,
+        email = user.email,
+        phoneNumber = user.phoneNumber,
+        imageUrl = user.image?.url ?: "default.jpg"
+    )
+}
