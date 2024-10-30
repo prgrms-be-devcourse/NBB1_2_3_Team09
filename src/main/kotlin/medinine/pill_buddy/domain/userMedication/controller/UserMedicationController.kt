2d7754027d80a4b7ce9f838ecd510049899ffcb8
xxxkyeom <seonggyeomgim97@gmail.com>
2024-10-30 14:34:46 +0900

    feat: 사용자 약 정보 컨트롤러 마이그레이션

--- src/main/kotlin/medinine/pill_buddy/domain/userMedication/controller/UserMedicationController.kt
+++ src/main/kotlin/medinine/pill_buddy/domain/userMedication/controller/UserMedicationController.kt
@@ -5,11 +5,7 @@ import medinine.pill_buddy.domain.userMedication.dto.UserMedicationDTO
 import medinine.pill_buddy.domain.userMedication.service.UserMedicationService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +15,7 @@ class UserMedicationController (
 ){
     @PostMapping
     fun createUserMedication(
-        @PathVariable caretakerId: Long,
-        @RequestBody userMedicationDTO: UserMedicationDTO
+        @PathVariable caretakerId: Long, @RequestBody userMedicationDTO: UserMedicationDTO
     ): ResponseEntity<UserMedicationDTO> {
         val savedUserMedication = userMedicationService.register(caretakerId, userMedicationDTO)
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUserMedication)
