f433c3808164e5019ea7050eb5b1ee16c6611a8d
hongjeZZ <ajyong316@gmail.com>
2024-10-30 12:42:07 +0900

    feat: Redis 설정 클래스 및 Jwt blackList RedisTemplate 구현

---
+++ src/main/kotlin/medinine/pill_buddy/global/redis/RedisUtils.kt
@@ -0,0 +1,22 @@
+package medinine.pill_buddy.global.redis
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+
+
+@Component
+class RedisUtils(
+    @Qualifier(value = "BlackList") private val redisTemplate: RedisTemplate<String, String>
+) {
+
+    fun setBlackList(token: String, loginId: String, milliSeconds: Long) {
+        redisTemplate.opsForValue().set(token, loginId, milliSeconds, TimeUnit.MILLISECONDS)
+    }
+
+    fun hasTokenBlackList(token: String): Boolean {
+        return redisTemplate.hasKey(token)
+    }
+}
