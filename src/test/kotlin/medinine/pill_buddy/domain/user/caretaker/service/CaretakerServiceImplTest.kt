e1a328237f07f400c745805cce88eb971e15151e
xxxkyeom <seonggyeomgim97@gmail.com>
2024-10-31 15:24:25 +0900

    test: 보호자 등록 기능 서비스, 컨트롤러 레이어 테스트 코드

---
+++ src/test/kotlin/medinine/pill_buddy/domain/user/caretaker/service/CaretakerServiceImplTest.kt
@@ -0,0 +1,28 @@
+package medinine.pill_buddy.domain.user.caretaker.service
+
+import medinine.pill_buddy.domain.user.caretaker.dto.CaretakerCaregiverDTO
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.Test
+
+@SpringBootTest
+class CaretakerServiceImplTest {
+
+    @Autowired
+    private lateinit var caretakerService: CaretakerService
+
+    @Test
+    @DisplayName("보호자 등록 테스트")
+    fun addCaregiverTest() {
+        val caretakerId = 2L
+        val caregiverId = 1L
+
+        val register: CaretakerCaregiverDTO = caretakerService.register(caretakerId, caregiverId)
+
+        assertThat(register.caretaker?.id).isEqualTo(2L)
+        assertThat(register.caregiver?.id).isEqualTo(1L)
+    }
+}
