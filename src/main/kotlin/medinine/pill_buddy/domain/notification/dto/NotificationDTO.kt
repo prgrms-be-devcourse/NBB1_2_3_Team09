6d2978f4e944e64a3dbf0fe3f61788866bdda2fe
MisaSohee <sohee5951@gmail.com>
2024-10-31 15:04:44 +0900

    feat: NotificationDTO 구현

---
+++ src/main/kotlin/medinine/pill_buddy/domain/notification/dto/NotificationDTO.kt
@@ -0,0 +1,27 @@
+package medinine.pill_buddy.domain.notification.dto
+
+import medinine.pill_buddy.domain.notification.entity.Notification
+import medinine.pill_buddy.domain.userMedication.entity.Frequency
+import java.time.LocalDateTime
+
+data class NotificationDTO(
+    val notificationId: Long? = null,
+    val medicationName: String? = null,
+    val frequency: Frequency? = null,
+    val notificationTime: LocalDateTime? = null,
+    val caretakerId: Long? = null
+) {
+    constructor(notification: Notification) : this(
+        notificationId = notification.id,
+        medicationName = notification.userMedication?.name,
+        frequency = notification.userMedication?.frequency,
+        notificationTime = notification.notificationTime,
+        caretakerId = notification.caretaker?.id
+    )
+
+    companion object {
+        fun convertToDTOs(notifications: List<Notification>): List<NotificationDTO> {
+            return notifications.map { NotificationDTO(it) }
+        }
+    }
+}
