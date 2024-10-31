151f4628665a8fb6b20acc13af5ac0635b1aa2df
xxxkyeom <seonggyeomgim97@gmail.com>
2024-10-31 15:25:26 +0900

    feat: 사용자 서비스 레이어 마이그레이션

---
+++ src/main/kotlin/medinine/pill_buddy/domain/user/caretaker/service/CaretakerService.kt
@@ -0,0 +1,7 @@
+package medinine.pill_buddy.domain.user.caretaker.service
+
+import medinine.pill_buddy.domain.user.caretaker.dto.CaretakerCaregiverDTO
+
+interface CaretakerService {
+    fun register(caretakerId: Long, caregiverId: Long): CaretakerCaregiverDTO
+}
