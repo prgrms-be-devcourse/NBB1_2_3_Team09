26d6b4f69ed8dd38c81fd1051c50942c05020385
MisaSohee <sohee5951@gmail.com>
2024-10-31 15:03:18 +0900

    feat: CaretakerCaregiverRepository 구현 - findByCaretaker 메서드: caretaker로 CaretakerCaregiver 객체 검색

---
+++ src/main/kotlin/medinine/pill_buddy/domain/user/caretaker/repository/CaretakerCaregiverRepository.kt
@@ -0,0 +1,9 @@
+package medinine.pill_buddy.domain.user.caretaker.repository
+
+import medinine.pill_buddy.domain.user.caretaker.entity.Caretaker
+import medinine.pill_buddy.domain.user.caretaker.entity.CaretakerCaregiver
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CaretakerCaregiverRepository : JpaRepository<CaretakerCaregiver, Long> {
+    fun findByCaretaker(caretaker: Caretaker): List<CaretakerCaregiver>
+}
