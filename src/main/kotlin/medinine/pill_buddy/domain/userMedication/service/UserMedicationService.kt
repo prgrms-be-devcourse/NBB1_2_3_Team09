ccef521b32e4e88c4a9aa3d424ac7a8c118eb1f5
xxxkyeom <seonggyeomgim97@gmail.com>
2024-11-01 16:49:56 +0900

    30: [FEAT] 사용자 약 복용 여부 확인 (#36)
    * test: 사용자 약 복용 여부 확인 테스트 코드
    
    * feat: 사용자 약 복용 여부 서비스 레이어 마이그레이션
    
    * feat: Record 엔티티의 dto 클래스 마이그레이션
    
    * feat: 사용자 약 복용 여부 컨트롤러 레이어 마이그레이션

--- src/main/kotlin/medinine/pill_buddy/domain/userMedication/service/UserMedicationService.kt
+++ src/main/kotlin/medinine/pill_buddy/domain/userMedication/service/UserMedicationService.kt
@@ -1,9 +1,12 @@
 package medinine.pill_buddy.domain.userMedication.service
 
+import medinine.pill_buddy.domain.record.dto.RecordDTO
 import medinine.pill_buddy.domain.userMedication.dto.UserMedicationDTO
+import java.time.LocalDateTime
 
 interface UserMedicationService {
     fun register(caretakerId: Long, userMedicationDTO: UserMedicationDTO): UserMedicationDTO
     fun retrieve(caretakerId: Long): List<UserMedicationDTO>
     fun modify(caretakerId: Long, userMedicationId: Long, userMedicationDTO: UserMedicationDTO): UserMedicationDTO
+    fun getUserMedicationRecordsByDate(caretakerId: Long, date: LocalDateTime): List<RecordDTO>
 }
