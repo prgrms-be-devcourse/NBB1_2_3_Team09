1affd240c661208eb5913f633091f404cf50f1c3
xxxkyeom <seonggyeomgim97@gmail.com>
2024-11-01 16:28:39 +0900

    feat: 사용자 약 복용 여부 컨트롤러 레이어 마이그레이션

--- src/main/kotlin/medinine/pill_buddy/domain/user/caretaker/controller/CaretakerController.kt
+++ src/main/kotlin/medinine/pill_buddy/domain/user/caretaker/controller/CaretakerController.kt
@@ -1,12 +1,17 @@
 package medinine.pill_buddy.domain.user.caretaker.controller
 
 import lombok.RequiredArgsConstructor
+import medinine.pill_buddy.domain.record.dto.RecordDTO
 import medinine.pill_buddy.domain.user.caretaker.dto.CaretakerCaregiverDTO
 import medinine.pill_buddy.domain.user.caretaker.service.CaretakerService
+import medinine.pill_buddy.domain.userMedication.entity.MedicationType
 import medinine.pill_buddy.domain.userMedication.service.UserMedicationService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
 
 @RestController
 @RequiredArgsConstructor
@@ -24,11 +29,20 @@ class CaretakerController(
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCaretakerCaregiverDTO)
     }
 
-    @DeleteMapping("/{caretakerId}/caregivers/{caregiverId}")
+    @DeleteMapping("/{caretakerId}/caregivers/{caregiverId}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun deleteCaregiver(
         @PathVariable caretakerId: Long, @PathVariable caregiverId: Long
     ): ResponseEntity<Map<String, String>> {
         caretakerService.remove(caretakerId, caregiverId)
         return ResponseEntity.ok(mapOf("Process" to "Success"))
+    }
+
+    @GetMapping("/{caretakerId}/user-medications/records")
+    fun getRecordsByDate(
+        @PathVariable caretakerId: Long,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate
+    ): ResponseEntity<List<RecordDTO>> {
+        val records = userMedicationService.getUserMedicationRecordsByDate(caretakerId, date.atStartOfDay())
+        return ResponseEntity.ok(records)
     }
 }
