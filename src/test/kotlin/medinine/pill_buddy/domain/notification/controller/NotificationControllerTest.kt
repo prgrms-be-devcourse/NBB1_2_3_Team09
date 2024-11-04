43efb407d697c4acd91943608a3c7d38aecdfa67
MisaSohee <sohee5951@gmail.com>
2024-11-04 16:45:42 +0900

    feat: 알림 시간 수정 컨트롤러 테스트 구현

--- src/test/kotlin/medinine/pill_buddy/domain/notification/controller/NotificationControllerTest.kt
+++ src/test/kotlin/medinine/pill_buddy/domain/notification/controller/NotificationControllerTest.kt
@@ -1,6 +1,8 @@
 package medinine.pill_buddy.domain.notification.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import medinine.pill_buddy.domain.notification.dto.NotificationDTO
+import medinine.pill_buddy.domain.notification.dto.UpdateNotificationDTO
 import medinine.pill_buddy.domain.notification.service.NotificationService
 import medinine.pill_buddy.domain.userMedication.entity.Frequency
 import org.junit.jupiter.api.DisplayName
@@ -12,8 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.time.LocalDateTime
@@ -26,14 +27,17 @@ class NotificationControllerTest(
     @MockBean
     private lateinit var notificationService: NotificationService
 
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
     private val BASE_URL = "/api/notification"
+    private val fixedTime = LocalDateTime.of(2024, 11, 1, 9, 0, 0)
 
     @Test
     @DisplayName("알림 생성")
     fun createNotifications_test() {
         // given
         val userMedicationId = 1L
-        val fixedTime = LocalDateTime.of(2024, 11, 1, 9, 0, 0)
         val notificationDTO = NotificationDTO(
             notificationId = 1L,
             notificationTime = fixedTime,
@@ -75,7 +79,6 @@ class NotificationControllerTest(
     @DisplayName("알림 조회")
     fun findNotifications_test() {
         // given
-        val fixedTime = LocalDateTime.of(2024, 11, 1, 9, 0, 0)
         val caretakerId = 1L
         val notificationDTO = NotificationDTO(
             notificationId = 1L,
@@ -107,6 +110,46 @@ class NotificationControllerTest(
                     "frequency": "ONCE_A_DAY"
                 }
             ]
+            """))
+    }
+
+    @Test
+    @DisplayName("알림 시간 수정")
+    fun updateNotification_test() {
+        // given
+        val notificationId = 1L
+        val updateNotificationDTO = UpdateNotificationDTO(
+            notificationTime = fixedTime.plusDays(1)
+        )
+        val notificationDTO = NotificationDTO(
+            notificationId = notificationId,
+            notificationTime = fixedTime.plusDays(1),
+            caretakerId = 1L,
+            caretakerUsername = "사용자",
+            medicationName = "Medication A",
+            frequency = Frequency.ONCE_A_DAY
+        )
+
+        `when`(notificationService.updateNotification(notificationId, updateNotificationDTO))
+            .thenReturn(notificationDTO)
+
+        // when & then
+        mvc.perform(
+            patch("$BASE_URL/$notificationId")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateNotificationDTO))
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().json("""
+                {
+                    "notificationId": 1,
+                    "notificationTime": "2024-11-02T09:00:00",
+                    "caretakerId": 1,
+                    "caretakerUsername": "사용자",
+                    "medicationName": "Medication A",
+                    "frequency": "ONCE_A_DAY"
+                }
             """))
     }
 }
