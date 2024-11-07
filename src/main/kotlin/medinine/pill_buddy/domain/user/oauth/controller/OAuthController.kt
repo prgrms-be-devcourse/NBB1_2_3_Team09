de391a8558962a936ba8d3c23f725d518de0a8cd
hongjeZZ <ajyong316@gmail.com>
2024-11-07 10:17:36 +0900

    refactor: OAuthController 리팩토링
    - 프론트 구현 로직 삭제
    - api 로직으로 리팩토링

--- src/main/kotlin/medinine/pill_buddy/domain/user/oauth/controller/OAuthController.kt
+++ src/main/kotlin/medinine/pill_buddy/domain/user/oauth/controller/OAuthController.kt
@@ -6,14 +6,12 @@ import lombok.RequiredArgsConstructor
 import medinine.pill_buddy.domain.user.dto.UserType
 import medinine.pill_buddy.domain.user.oauth.service.SocialLoginService
 import medinine.pill_buddy.global.jwt.JwtToken
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import java.net.URI
 
 
 @Controller
@@ -29,11 +27,7 @@ class OAuthController(
     fun getConnectionByKakao(@RequestParam userType: UserType): ResponseEntity<String> {
         val location: String = socialLoginService.getConnectionUrl(userType, "kakao")
 
-//        return ResponseEntity.ok(location)
-        return ResponseEntity.status(HttpStatus.FOUND)
-            .location(URI.create(location))
-            .build()
-
+        return ResponseEntity.ok(location)
     }
 
     @Operation(description = "사용자는 네이버 소셜 로그인을 할 수 있다.")
@@ -41,22 +35,9 @@ class OAuthController(
     fun getConnectionByNaver(@RequestParam userType: UserType): ResponseEntity<String> {
         val location: String = socialLoginService.getConnectionUrl(userType, "naver")
 
-//        return ResponseEntity.ok(location)
-        return ResponseEntity.status(HttpStatus.FOUND)
-            .location(URI.create(location))
-            .build()
+        return ResponseEntity.ok(location)
     }
 
-//    @GetMapping("/login/{registrationId}/{userType}")
-//    fun login(
-//        @RequestParam code: String,
-//        @PathVariable userType: String,
-//        @PathVariable registrationId: String
-//    ): ResponseEntity<JwtToken> {
-//        val jwtToken: JwtToken = socialLoginService.login(code, UserType.from(userType), registrationId)
-//
-//        return ResponseEntity.ok(jwtToken)
-//    }
     @GetMapping("/login/{registrationId}/{userType}")
     fun login(
         @RequestParam code: String,
@@ -65,9 +46,6 @@ class OAuthController(
     ): ResponseEntity<JwtToken> {
         val jwtToken: JwtToken = socialLoginService.login(code, UserType.from(userType), registrationId)
 
-        val redirectUrl = "/afterLogin.html?token=${jwtToken.accessToken}"
-        return ResponseEntity.status(HttpStatus.FOUND)
-            .location(URI.create(redirectUrl))
-            .build()
+        return ResponseEntity.ok(jwtToken)
     }
 }
