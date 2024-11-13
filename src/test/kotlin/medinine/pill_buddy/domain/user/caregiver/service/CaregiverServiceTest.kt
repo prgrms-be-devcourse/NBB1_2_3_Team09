2ff5f66f9b430ac9655d5b30e99bc02f5e256391
jinwoo <dlwlsdn2628@hanmail.net>
2024-11-13 14:00:20 +0900

    .

--- src/test/kotlin/medinine/pill_buddy/domain/user/caregiver/service/CaregiverServiceTest.kt
+++ src/test/kotlin/medinine/pill_buddy/domain/user/caregiver/service/CaregiverServiceTest.kt
@@ -1,36 +1,53 @@
 package medinine.pill_buddy.domain.user.caregiver.service
 
 import medinine.pill_buddy.domain.user.caregiver.entity.Caregiver
-import medinine.pill_buddy.domain.user.caretaker.dto.CaretakerCaregiverDTO
+import medinine.pill_buddy.domain.user.caregiver.repository.CaregiverRepository
 import medinine.pill_buddy.domain.user.caretaker.entity.Caretaker
-import medinine.pill_buddy.domain.userMedication.dto.UserMedicationDTO
-import medinine.pill_buddy.domain.userMedication.entity.Frequency
-import medinine.pill_buddy.domain.userMedication.entity.MedicationType
-import medinine.pill_buddy.domain.userMedication.entity.UserMedication
+import medinine.pill_buddy.domain.user.caretaker.entity.CaretakerCaregiver
+import medinine.pill_buddy.domain.user.caretaker.repository.CaretakerCaregiverRepository
+import medinine.pill_buddy.domain.user.caretaker.repository.CaretakerRepository
 import medinine.pill_buddy.domain.userMedication.repository.UserMedicationRepository
+import medinine.pill_buddy.global.exception.ErrorCode
 import medinine.pill_buddy.global.exception.PillBuddyCustomException
-import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.assertThatThrownBy
-import org.junit.jupiter.api.BeforeEach
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDateTime
-import kotlin.test.Test
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.mockito.BDDMockito.*
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
-@SpringBootTest
-class CaregiverServiceTest @Autowired constructor(
-    private val caregiverService: CaregiverService,
-    private val userMedicationRepository: UserMedicationRepository
-) {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CaregiverServiceTest {
+
+    @Mock
+    private lateinit var caretakerCaregiverRepository: CaretakerCaregiverRepository
+
+    @Mock
+    private lateinit var caregiverRepository: CaregiverRepository
+
+    @Mock
+    private lateinit var caretakerRepository: CaretakerRepository
+
+    @Mock
+    private lateinit var userMedicationRepository: UserMedicationRepository
+
+    @InjectMocks
+    private lateinit var caregiverService: CaregiverService
 
     private lateinit var caregiver: Caregiver
     private lateinit var caretaker: Caretaker
+    private lateinit var caretakerCaregiver: CaretakerCaregiver
+
+    @BeforeAll
+    fun setUpMocks() {
+        MockitoAnnotations.openMocks(this)
+    }
 
     @BeforeEach
-    @Transactional
     fun setUp() {
         caregiver = Caregiver(
+            id = 1L,
             username = "test-caregiver",
             loginId = "caregiver-login",
             password = "caregiver-password",
@@ -39,6 +56,7 @@ class CaregiverServiceTest @Autowired constructor(
         )
 
         caretaker = Caretaker(
+            id = 2L,
             username = "test-caretaker",
             loginId = "caretaker-login",
             password = "caretaker-password",
@@ -46,56 +64,84 @@ class CaregiverServiceTest @Autowired constructor(
             phoneNumber = "010-2222-2222"
         )
 
-        val medication = UserMedication(
-            name = "Aspirin",
-            dosage = 10,
-            frequency = Frequency.TWICE_A_DAY,
-            type = MedicationType.MEDICATION,
-            stock = 10,
-            expirationDate = LocalDateTime.now().plusYears(3),
-            startDate = LocalDateTime.now(),
-            endDate = LocalDateTime.now().plusDays(5),
-            caretaker = caretaker
+        caretakerCaregiver = CaretakerCaregiver(
+            id = 1L,
+            caretaker = caretaker,
+            caregiver = caregiver
         )
-        userMedicationRepository.save(medication)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        reset(caretakerCaregiverRepository, caregiverRepository, caretakerRepository, userMedicationRepository)
     }
 
     @Test
-    @Transactional
-    fun getCaretakerMedicationsTests1() {   //정상적으로 불러지는지 확인
-        val medications: List<UserMedicationDTO> = caregiverService.getCaretakerMedications(caregiver.id!!, caretaker.id!!)
-        assertThat(medications[0].name).isEqualTo("Aspirin")
+    @DisplayName("caretaker 등록")
+    fun registerTest() {
+        // given
+        given(caretakerRepository.findById(caretaker.id!!)).willReturn(java.util.Optional.of(caretaker))
+        given(caregiverRepository.findById(caregiver.id!!)).willReturn(java.util.Optional.of(caregiver))
+        given(caretakerCaregiverRepository.findByCaretakerIdAndCaregiverId(caretaker.id!!, caregiver.id!!))
+            .willReturn(null)
+        given(caretakerCaregiverRepository.save(any(CaretakerCaregiver::class.java)))
+            .willReturn(caretakerCaregiver)
+
+        // when
+        val result = caregiverService.register(caregiver.id!!, caretaker.id!!)
+
+        // then
+        assertNotNull(result)
+        assertEquals(caretakerCaregiver.id, result.id)
+        assertEquals(caretakerCaregiver.caretaker, result.caretaker)
+        assertEquals(caretakerCaregiver.caregiver, result.caregiver)
     }
 
     @Test
-    @Transactional
-    fun getCaretakerMedicationsTests2() {   //예외 - 등록되지 않은 사용자 검색했을 때
-        val invalidCaretakerId = 99L
-        assertThatThrownBy { caregiverService.getCaretakerMedications(caregiver.id!!, invalidCaretakerId) }
-            .isInstanceOf(PillBuddyCustomException::class.java)
-            .hasMessage("사용자 정보가 일치하지 않습니다")
+    @DisplayName("이미 등록된 caretaker 등록")
+    fun registerThrowExceptionTest() {
+        // given
+        given(caretakerRepository.findById(caretaker.id!!)).willReturn(java.util.Optional.of(caretaker))
+        given(caregiverRepository.findById(caregiver.id!!)).willReturn(java.util.Optional.of(caregiver))
+        given(caretakerCaregiverRepository.findByCaretakerIdAndCaregiverId(caretaker.id!!, caregiver.id!!))
+            .willReturn(caretakerCaregiver)
+
+        // when
+        val exception = assertThrows<PillBuddyCustomException> {
+            caregiverService.register(caregiver.id!!, caretaker.id!!)
+        }
+
+        // then
+        assertEquals(ErrorCode.CARETAKER_ALREADY_REGISTERED, exception.errorCode)
     }
 
     @Test
-    @Transactional
-    fun caregiverServiceTests1() {
-        assertThatThrownBy { caregiverService.register(caregiver.id!!, caretaker.id!!) }
-            .isInstanceOf(PillBuddyCustomException::class.java)
-            .hasMessage("이미 등록된 사용자 정보입니다")
-    }
+    @DisplayName("caretaker 삭제")
+    fun removeTest() {
+        // given
+        given(caretakerCaregiverRepository.findByCaretakerIdAndCaregiverId(caretaker.id!!, caregiver.id!!))
+            .willReturn(caretakerCaregiver)
 
-    @Test
-    @Transactional
-    fun caregiverServiceTests2() {
-        val newCaregiverId = 2L
-        val register: CaretakerCaregiverDTO = caregiverService.register(newCaregiverId, caretaker.id!!)
-
-        assertThat(register.caregiver?.id).isEqualTo(newCaregiverId)
-        assertThat(register.caretaker?.id).isEqualTo(caretaker.id)
-    }
-
-    @Test
-    fun caregiverServiceTests3() {
+        // when
         caregiverService.remove(caregiver.id!!, caretaker.id!!)
+
+        // then
+        verify(caretakerCaregiverRepository).delete(caretakerCaregiver)
+    }
+
+    @Test
+    @DisplayName("caretaker 삭제 - 예외")
+    fun removeThrowExceptionTest() {
+        // given
+        given(caretakerCaregiverRepository.findByCaretakerIdAndCaregiverId(caretaker.id!!, caregiver.id!!))
+            .willReturn(null)
+
+        // when
+        val exception = assertThrows<PillBuddyCustomException> {
+            caregiverService.remove(caregiver.id!!, caretaker.id!!)
+        }
+
+        // then
+        assertEquals(ErrorCode.CAREGIVER_CARETAKER_NOT_MATCHED, exception.errorCode)
     }
 }
